242d5e1e6904120d2064b9f88ed347d182ce21c2
Sadie <reallifesadie@gmail.com>
2024-07-13 04:03:09 -0600

    Spicy Site Stuff

---
+++ api_modules/spicybackend.js
@@ -0,0 +1,11 @@
+// GET – Allows you to retrieve a list or collection
+// POST – Helps you in creating a  new item in collection
+// PUT –Replaces an item with another item in parameters
+// PATCH – Enables you to update an item with information in parameters
+// DELETE – Enables you to remove item or collection
+
+module.exports.run = (api, req,res,db) => {
+    api.loadapi("./api_modules");
+    
+    res.send(JSON.stringify({data: "WIP!"}))
+}
---
+++ public/js/spicy.js
@@ -0,0 +1,4 @@
+var main = document.getElementsByClassName("main")[0]
+let test2 = document.createElement("h1")
+test2.innerHTML = "Test2"
+main.appendChild(test2)

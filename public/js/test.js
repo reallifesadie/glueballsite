140861e88ba7802ef5dca3e23f6dfcbe39e07d99
Sadie <38514929+reallifesadie@users.noreply.github.com>
2024-08-12 11:25:25 -0600

    Test page for the api is functional!

--- public/js/test.js
+++ public/js/test.js
@@ -1,21 +1,28 @@
-let main = document.getElementsByClassName('main')[0];
-var clicky = () => {
-    fetch("/api/login", {
-        method: 'POST',
+let output = document.getElementById("output");
+let fouff = document.getElementById("sendthing");
+
+fouff.addEventListener("submit", (event) => {
+    event.preventDefault();
+    thingbox = {
+        method: fouff[1].value,
         headers: {
           'Content-Type': 'application/json'
           // Add any other headers as needed
-        },
-        body: JSON.stringify({data: {}})
-    }).then(response => {
+        }
+    }
+    if(fouff[1].value != "GET") {
+        console.log(fouff[1].value);
+        thingbox.body = JSON.stringify(fouff[2]);
+    }
+    fetch(fouff[0].value, thingbox).then(response => {
         if (!response.ok) throw new Error('Network response was not ok');
         return response.json();
     }).then(data => {
         let test2 = document.createElement("p");
         test2.innerHTML = data.data
-        main.appendChild(test2)
+        output.appendChild(test2)
         console.log(data.data)
     }).catch(error => {
         console.error('Error:', error);
     });
-}
+});

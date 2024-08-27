de0fd1c90b23cda0258a931cd1f12a9a99b50103
Sadie <38514929+reallifesadie@users.noreply.github.com>
2024-08-27 15:23:12 -0600

    Zippo Swappo

--- api_modules/login.js
+++ api_modules/login.js
@@ -12,28 +12,45 @@
 // DELETE – Enables you to remove item or collection
 //  - Delete Account
 
+//For the hashing stuff
 const bcrypt = require("bcrypt");
+const { response } = require("express");
+const saltRounds = 10;
 
-module.exports.run = (api, req,res,db) => {
+// A function that will return false if the list provided has undefined or empty vars
+const ZippoSwappo = (funkyList) => {
+    for(let i = 0; i < funkyList.length; i++) {
+        if(!funkyList[i] || funkyList[i] == "") {
+            return false;
+        }
+    }
+    return true;
+}
+module.exports.run = (api,req,res,db) => {
     switch(req.method) {
         case "POST":
-            console.log(req.body.data)
-            let funkyList = [req.body.data.username, req.body.data.displayname, req.body.data.password]
-            for(let i = 0; i < funkyList.length; i++) {
-                if(!funkyList[i] || funkyList[i] == "") {
-                    res.send(JSON.stringify({data: "Please include INFORMATINONN!"}));
-                    break;
-                }
-                console.log(i)
+            // ZIPPO SWAPPO 
+            if(!ZippoSwappo([req.body.data.username, req.body.data.displayname, req.body.data.password])) {
+                res.send(JSON.stringify({data: "Please include username password and displayname!"}));
+                break;
             }
+            db.get("SELECT username FROM spicy WHERE username = ?", [req.body.username], (err, row) => {
+                if(err) console.error(err);
+                console.log(row);
+            });
+            // bcrypt.hash(req.body.data.password, saltRounds, function(err, hash) {
+            //     if(!err) {
+            //         db.run('INSERT INTO users (username, password) VALUES (?,?)', [req.body.data.username, hash], (err, row) => {
+            //             if(!err) {
+            //                 console.log("New user 'root' created!");
+            //             } else console.error(err);
+            //         });
+            //     } else console.error(err);
+            // });
             res.send(JSON.stringify({data: "OK! 👍"}));
             break;
         case "GET":
             res.send(JSON.stringify({data: "Test2!"}));
             break;
     }
-}
-
-module.exports.help = {
-    
 }

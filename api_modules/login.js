bb009451e1af9823cb493d77a84cd9c9d36c89ca
Sadie <reallifesadie@gmail.com>
2024-09-30 11:22:53 -0600

    Upgrades people upgrades

--- api_modules/login.js
+++ api_modules/login.js
@@ -15,7 +15,9 @@
 //For the hashing stuff
 const bcrypt = require("bcrypt");
 const { response } = require("express");
+const { CONSTRAINT } = require("sqlite3");
 const saltRounds = 10;
+
 
 // A function that will return false if the list provided has undefined or empty vars
 const ZippoSwappo = (funkyList) => {
@@ -28,6 +30,9 @@ const ZippoSwappo = (funkyList) => {
 }
 module.exports.run = (api,req,res,db) => {
     switch(req.method) {
+        case "GET":
+            res.send(JSON.stringify({data: "Test2!"}));
+            break;
         case "POST":
             // ZIPPO SWAPPO 
             if(!ZippoSwappo([req.body.data.username, req.body.data.displayname, req.body.data.password])) {
@@ -37,12 +42,18 @@ module.exports.run = (api,req,res,db) => {
             db.get("SELECT username FROM users WHERE username = ?", [req.body.data.username], (err, row) => {
                 if(err) return console.error(err);
                 if(row == undefined) {
-                    bcrypt.hash(req.body.data.password, saltRounds, function(err, hash) {
+                    bcrypt.hash(req.body.data.password, saltRounds, (err, hash) => {
                         if(err) return console.error(err);
-                        db.run('INSERT INTO users (username, password) VALUES (?,?)', [req.body.data.username, hash], (err, row) => {
+                        let porgy = Math.round(0xffffff * Math.random()).toString(16)
+                        db.run('INSERT INTO users (username, password, name, code) VALUES (?,?,?,?)', [
+                            req.body.data.username,
+                            hash,
+                            req.body.data.displayname,
+                            `${porgy}`
+                        ], (err) => {
                             if(err) return console.error(err);
                             console.log(`New user ${req.body.data.username} created!`);
-                            res.send(JSON.stringify({data: `New user ${req.body.data.username} created!`}));
+                            res.send(JSON.stringify({data: `New user ${req.body.data.username} created!`, code: porgy}));
                         });
                     });
                 } else {
@@ -51,8 +62,34 @@ module.exports.run = (api,req,res,db) => {
             });
             
             break;
-        case "GET":
-            res.send(JSON.stringify({data: "Test2!"}));
+        case "PUT":
+            if(!ZippoSwappo([req.body.data.username, req.body.data.password])) {
+                res.send(JSON.stringify({data: "Please include username and password!"}));
+                break;
+            }
+            db.get("SELECT username, password, code FROM users WHERE username = ?", [req.body.data.username], (err, row) => {
+                if (err) return console.error(err);
+                if (row == undefined) {
+                    res.send(JSON.stringify({data: 'Invalid password for user!'}));
+                    return true;
+                }
+                bcrypt.compare(req.body.data.password, row.password, (err, result) => {
+                    if (err) return console.error(err);
+                    if(result) {
+                        if(row.code == null || row.code == "") {
+                            let porgy = Math.round(0xffffff * Math.random()).toString(16)
+                            db.run('UPDATE users SET code = ? WHERE username = ?', [`${porgy}`, req.body.data.username], (err, row) => {
+                                if(err) return console.error(err);
+                                res.send(JSON.stringify({data: "User loged in", code: porgy}));
+                            });
+                        } else {
+                            res.send(JSON.stringify({data: row.code}));
+                        }
+                    } else {
+                        res.send(JSON.stringify({data: 'Invalid password for user!'}));
+                    }
+                });
+            });
             break;
     }
 }
